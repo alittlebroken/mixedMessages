5f7bbfa288cdc3941051bcbadc6c96e5bbc5ca96
Paul Lockyer <plockyer@googlemail.com>
2021-07-07 17:07:41 +0100

    Added methods to generate random words and create the final message

--- mixedmessages.js
+++ mixedmessages.js
@@ -14,6 +14,7 @@ const mixedMessage = {
     actions: [],
     subjects: [],
   },
+
   // Getter and setters
   get entities(){
     return this._words["entities"];
@@ -21,16 +22,37 @@ const mixedMessage = {
   set entities(entity){
     this._words["entities"].push(entity);
   },
+
   get actions(){
     return this._words["actions"];
   },
   set actions(action){
     this._words["actions"].push(action);
   },
+
   get subjects(){
     return this._words["subjects"];
   },
   set subjects(subject){
     this._words["subjects"].push(subjects);
   },
-}
+
+  // grab a random word from an array
+  randomWord(word_array){
+    let len = this._words[word_array].length;
+    if(len > 0){
+      return this._words[word_array][Math.floor(Math.random() * len)];
+    }
+  },
+
+  // create the news headline and return it
+  createMessage(){
+    const entity = this.randomWord('entities');
+    const action = this.randomWord('actions');
+    const subject = this.randomWord('subjects');
+    return `${entity} was ${action} today due to ${subject}`;
+  },
+
+};
+
+console.log(mixedMessage);
