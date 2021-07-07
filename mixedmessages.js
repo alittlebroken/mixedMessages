b0e8227c79406415f5e7455631621fab5abdce60
Paul Lockyer <plockyer@googlemail.com>
2021-07-07 15:06:48 +0100

    Added an object of arrays to hold words as well as getters and setters for them

--- mixedmessages.js
+++ mixedmessages.js
@@ -7,4 +7,30 @@
 */
 
 // Main message object
-const mixedMessage = {}
+const mixedMessage = {
+  // Store words used to generate the mixedmessage headline
+  _words: {
+    entities: [],             // This could be people or groups
+    actions: [],
+    subjects: [],
+  },
+  // Getter and setters
+  get entities(){
+    return this._words["entities"];
+  },
+  set entities(entity){
+    this._words["entities"].push(entity);
+  },
+  get actions(){
+    return this._words["actions"];
+  },
+  set actions(action){
+    this._words["actions"].push(action);
+  },
+  get subjects(){
+    return this._words["subjects"];
+  },
+  set subjects(subject){
+    this._words["subjects"].push(subjects);
+  },
+}
