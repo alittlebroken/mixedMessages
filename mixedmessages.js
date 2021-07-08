26e89e565edf88ba0e35069532a2fd1ec829a48c
Paul Lockyer <plockyer@googlemail.com>
2021-07-08 09:54:26 +0100

    Added data to the various word arrays

--- mixedmessages.js
+++ mixedmessages.js
@@ -8,6 +8,10 @@
 
 // Main message object
 const mixedMessage = {
+
+ // Build the message to be displayed
+  _message: '',
+
   // Store words used to generate the mixedmessage headline
   _words: {
     entities: [],             // This could be people or groups
@@ -55,6 +59,48 @@ const mixedMessage = {
 
 };
 
+// Add some data into the mixedMessageobject
+// People, groups, bodies
+mixedMessage['entities'] = 'Boris Johnson';
+mixedMessage['entities'] = 'Bono';
+mixedMessage['entities'] = 'Nasa';
+mixedMessage['entities'] = 'The England football team';
+mixedMessage['entities'] = 'Politicians';
+mixedMessage['entities'] = 'Animals';
+mixedMessage['entities'] = 'The Flat Earth Society';
+mixedMessage['entities'] = 'Scientists';
+mixedMessage['entities'] = 'Doctors';
+mixedMessage['entities'] = 'Soldiers';
+
+// actions
+mixedMessage['actions'] = 'cleared';
+mixedMessage['actions'] = 'arrested';
+mixedMessage['actions'] = 'discovered';
+mixedMessage['actions'] = 'buried';
+mixedMessage['actions'] = 'credited';
+mixedMessage['actions'] = 'invaded';
+mixedMessage['actions'] = 'murdered';
+mixedMessage['actions'] = 'captured';
+mixedMessage['actions'] = 'confirmed';
+mixedMessage['actions'] = 'denied';
+
+// subjects
+mixedMessage['subjects'] = 'aliens off the coast of France.';
+mixedMessage['subjects'] = 'oil fields in the middle east.';
+mixedMessage['subjects'] = 'smuggling ancient artifacts.';
+mixedMessage['subjects'] = 'a small group of children.';
+mixedMessage['subjects'] = 'the annual gathering of cat lovers.';
+mixedMessage['subjects'] = 'freedom of speech.';
+mixedMessage['subjects'] = 'cure for cancer.';
+mixedMessage['subjects'] = 'widespread pandemic hitting third world countries.';
+mixedMessage['subjects'] = 'a passion for dance.';
+mixedMessage['subjects'] = 'blackholes leading to another universe.';
+
+// show the user the final message
+console.log(mixedMessage.createMessage());
+
+// Uncomment the below to run some Tests
+/*
 console.log('Tests');
 console.log('1. Add some words ( 3 entries per array )')
 mixedMessage["entities"] = 'Nasa';
@@ -65,12 +111,16 @@ mixedMessage["actions"] = 'arrested';
 mixedMessage["actions"] = 'discovered';
 mixedMessage["actions"] = 'rehabilitated';
 console.log(mixedMessage["actions"]);
-mixedMessage["subjects"] = 'due to covid.';
-mixedMessage["subjects"] = 'due to bad parenting.';
-mixedMessage["subjects"] = 'as a consequence of money laundering.';
+mixedMessage["subjects"] = 'covid.';
+mixedMessage["subjects"] = 'bad parenting.';
+mixedMessage["subjects"] = 'a consequence of money laundering.';
 console.log(mixedMessage["subjects"]);
 
 console.log('2. Randomly select words - 1 random word per array');
 console.log(mixedMessage.randomWord('entities'));
 console.log(mixedMessage.randomWord('actions'));
 console.log(mixedMessage.randomWord('subjects'));
+
+console.log('3. Final message generation');
+console.log(mixedMessage.createMessage());
+*/
